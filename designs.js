bbef69ad0afef5f9c9bf7f62e23fa8c76ecd38ed
DCAckley <95905212+DCAckley@users.noreply.github.com>
2021-12-09 23:47:36 -0800

    Add files via upload

---
+++ designs.js
@@ -0,0 +1,36 @@
+//Project to set up a grid to make Pixel Art
+
+//define variables
+var height, width, color;
+
+
+//Call makeGrid when the grid height and width are submitted
+$('#sizePicker').submit(function (event){
+    event.preventDefault();
+    height=$('#inputHeight').val();
+    width=$('#inputWidth').val();
+    makeGrid(height,width);
+})
+
+//Set up the grid "canvas" as a table
+function makeGrid(height,width) {
+    //remove grid before creating a new one if one exists
+    $('tr').remove(); 
+    for (var i = 1; i <= height; i++) {
+        $('#pixelCanvas').append('<tr id=table' + i+'></tr>')
+        for (var j = 1; j <= width; j++) {
+            $('#table' + i).append('<td></td>')
+        }
+    }
+    //Add color when clicked & remove if clicked again
+    $('td').click(function addColor() {
+        color=$('#colorPicker').val();
+        
+        //"this" keyword refers to an object which is executing the current bit of javascript code
+        if ($(this).attr('style')){
+            $(this).removeAttr('style')
+       }    else{
+           $(this).attr('style', 'background-color:' + color);
+       }
+    })
+}
